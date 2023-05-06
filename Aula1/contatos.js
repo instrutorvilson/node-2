0f053c726d2c1322803c5962e8a7ca1fd87599cc
Gravação Proway <gravacao@proway.treina>
2023-05-06 16:29:39 -0300

    Adicionado rota de alterar e excluir contatos

--- Aula1/contatos.js
+++ Aula1/contatos.js
@@ -14,41 +14,12 @@ module.exports = {
         celular: req.body.celular
     }
     res.status(201).send(contato)
+   },
+   alterar(req, res){
+     res.status(201).send(`Alterado: ${req.params.idcontato}`)
+   },
+
+   excluir(req, res){
+    res.status(201).send(`Excluido: ${req.params.idcontato}`)
    }
 }
-/*
-app.get('/contato', (req, res) => {
-    res.status(200).send("lista de contatos")
-  })
-  
-
-  app.get('/contato/:idcontato', (req, res) => {
-      res.status(200).send(`idcontato: ${req.params.idcontato}`)
-    })
-  
-  app.post('/contato', (req, res) => {
-      let contato = {
-          nome: req.body.nome,
-          email: req.body.email,
-          celular: req.body.celular
-      }
-      res.status(201).send(contato)
-    })
-  
-    app.put('/contato/:idcontato', (req, res) => {
-     //pesquisar se existe o contato informado
-      let contato = {
-          id: req.params.idcontato,
-          nome: req.body.nome,
-          email: req.body.email,
-          celular: req.body.celular
-      }
-      res.status(201).send(contato)
-    })
-    
-    app.delete('/contato/:idcontato', (req, res) => {
-      //pesquisar se existe o contato informado
-      // res.status(204).send(`Contato ${req.params.idcontato} excluido com sucesso`)
-       res.status(204).send()
-     })
-  */
